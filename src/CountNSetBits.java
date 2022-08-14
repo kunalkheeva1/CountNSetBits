defe0b85b3182040bb58f73c2040bedef25e0115
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-08-13 21:06:28 -0700

    added the comments for the better understanding

--- src/CountNSetBits.java
+++ src/CountNSetBits.java
@@ -1,20 +1,34 @@
 public class CountNSetBits {
 
+    //count the n bits
     public static int countNSetBits(int n){
+        //if n is 0 then return 0
         if(n==0){
             return 0;
         }
+
+        //take p as a power and ans to return answer at the end
         int p=0;
         int ans = 0;
+
+
+        //until it becomes n or greater than it, increment it, and reduce by one after breaking the loop
+        // that is because we need the closest 2^p to n
         while(n>=Math.pow(2,p)){
             p++;
         }
         p--;
+
+        //pp is the number until which the formula works in the first step of the answer
         int pp =(int)Math.pow(2,p);
 
+        //second step is basically for the one column next of the bits(which is basically)
+        // the number - number where we found the bits already and add1
+        // then call the method again for the remaining bits at the bottom right
         ans = p*pp/2 + n-pp+1 + countNSetBits(n-pp);
         return ans;
     }
+    //https://youtu.be/g6OxU-hRGtY refer to this link for more details
 
 
     public static void main(String[] args) {
